3e06e0f8dc91362cba2d6fa6a5c381f70573a8eb
Joachim Bache-Mathiesen <joachimbm96@gmail.com>
2020-03-29 16:06:54 +0200

    Google API experiment

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version='0.1dev',
     packages=['MailNagger',],
     license='DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE',
-    long_description=open('README.md').read(),
+    long_description=open('README.md').read(), requires=['googleapiclient', 'google']
 )
