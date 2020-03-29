34e320a6b6794b4dce6ee7269a19bfa3836ae60e
Joachim Bache-Mathiesen <joachimbm96@gmail.com>
2020-03-29 11:21:45 +0200

    Project structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='MailNagger',
+    version='0.1dev',
+    packages=['MailNagger',],
+    license='DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE',
+    long_description=open('README.md').read(),
+)
