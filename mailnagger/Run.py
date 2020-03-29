410a0dbdc1a27c919cba1f89338716440c586ca1
Joachim Bache-Mathiesen <joachimbm96@gmail.com>
2020-03-29 21:18:07 +0200

    Tests

--- mailnagger/Run.py
+++ mailnagger/Run.py
@@ -1,6 +1,23 @@
 import tkinter as tk
+
+from mailnagger.message_service import create_message, send_message
+from mailnagger.quickstart import GoogleAuth
+
+
+def create_and_send():
+    google_service = GoogleAuth()
+    message = create_message('ewewewe.com', 'joachimbm96@gmail.com', 'Teeest', 'penis')
+    send_message(google_service.service, "me", message)
+
+
 window = tk.Tk(className='MailNagger')
 window.geometry("500x200")
-greeting = tk.Label(text="Hello World")
+frame = tk.Frame(window)
+frame.pack()
+greeting = tk.Label(text="Hello Big T")
 greeting.pack()
+slogan = tk.Button(frame,
+                   text="Send Mail",
+                   command=create_and_send)
+slogan.pack()
 window.mainloop()
--- mailnagger/quickstart.py
+++ mailnagger/quickstart.py
@@ -6,48 +6,29 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
 # If modifying these scopes, delete the file token.pickle.
-from mailnagger.message_service import create_message, send_message
-
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly', 'https://www.googleapis.com/auth/gmail.send']
 
-def main():
-    """Shows basic usage of the Gmail API.
-    Lists the user's Gmail labels.
-    """
-    creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
+class GoogleAuth:
 
-    service = build('gmail', 'v1', credentials=creds)
+    def __init__(self):
+        creds = None
 
-    # Call the Gmail API
-    results = service.users().labels().list(userId='me').execute()
-    labels = results.get('labels', [])
+        # The file token.pickle stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                creds = pickle.load(token)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'credentials.json', SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
 
-    if not labels:
-        print('No labels found.')
-    else:
-        print('Labels:')
-        for label in labels:
-            print(label['name'])
-
-    message = create_message('ewewewe.com', 't-hellum@hotmail.com', 'Teeest', 'penis')
-    send_message(service, "me", message)
-
-if __name__ == '__main__':
-    main()
+        self.service = build('gmail', 'v1', credentials=creds)
